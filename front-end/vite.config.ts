33dcc3d1253badebe1ee219aaa8c9916d9e3fb43
KimEuiSoo <dmltn3426@naver.com>
2026-06-01 14:58:36 +0900

    build : Docker build 세팅

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    watch: {
+      usePolling: true,    // ← Docker에서 파일 변경 감지에 필수
+      interval: 1000,
+    }
+  }
 })
